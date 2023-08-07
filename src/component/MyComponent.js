359488a3d08aa394aff6ab1c5aafeed986277b17
HuanNe <huan2605200210@gmail.com>
2023-08-07 17:23:19 +0700

    'baiso1'

---
+++ src/component/MyComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>Ny first Component</div>
+        );
+    }
+}
+export default MyComponent
